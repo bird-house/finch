a6523bc96bc59ac6ae303d57fae2408ff640c5ea
David Caron <dcaron05@gmail.com>
2019-05-01 23:44:25 -0400

    update tests

---
+++ tests/test_wps_bccaqv2_subset.py
@@ -0,0 +1,54 @@
+from unittest import mock
+
+import pytest
+from pathlib import Path
+
+from tests.utils import wps_literal_input, execute_process
+from finch.processes.wps_xsubset_bccaqv2 import SubsetBCCAQV2Process
+
+
+@mock.patch("finch.processes.wps_xsubset_bccaqv2.get_opendap_datasets")
+@mock.patch.object(SubsetBCCAQV2Process, "subsample")
+def test_bccaqv2_subset(mock_bccaq_subset, mock_datasets, client):
+    identifier = "subset_ensemble_BCCAQv2"
+    inputs = [
+        wps_literal_input("variable", "tasmin"),
+        wps_literal_input("rcp", "rcp26"),
+    ]
+
+    metalink = mock.MagicMock()
+    metalink.files = []
+
+    mock_datasets.return_value = ["dataset1", "dataset2"]
+    mock_bccaq_subset.return_value = metalink
+
+    outputs = execute_process(client, identifier, inputs, output_names=["zip"])
+
+    output_file = outputs[0]
+    assert len(outputs) == 1
+    assert output_file.endswith("zip")
+    assert Path(output_file).exists()
+
+    assert len(mock_bccaq_subset.call_args[0][0]["resource"]) == 2
+
+
+@pytest.mark.online
+def test_bccaqv2_subset_online(client):
+    identifier = "subset_ensemble_BCCAQv2"
+    up_right = 45.507485, -73.541295
+    bottom_left = 45.385644, -73.691963
+
+    inputs = [
+        wps_literal_input("variable", "tasmin"),
+        wps_literal_input("rcp", "rcp26"),
+        wps_literal_input("lon0", str(bottom_left[1])),
+        wps_literal_input("lon1", str(up_right[1])),
+        wps_literal_input("lat0", str(bottom_left[0])),
+        wps_literal_input("lat1", str(up_right[0])),
+        wps_literal_input("y0", "2010"),
+        wps_literal_input("y1", "2010"),
+    ]
+
+    outputs = execute_process(client, identifier, inputs, output_names=["zip"])
+
+    print(outputs)
--- tests/test_wps_xclim_indices.py
+++ tests/test_wps_xclim_indices.py
@@ -1,65 +1,7 @@
-from owslib.wps import WPSExecution
-
-import pytest
-from pywps import Service
-from pywps.tests import client_for, assert_response_success
-from xclim import temperature
 import xarray as xr
-from pywps import get_ElementMakerForVersion
-
 import finch
-from .common import get_output, CFG_FILE
+from tests.utils import wps_input_file, wps_literal_input, execute_process
 from finch.processes import make_xclim_indicator_process
-
-
-VERSION = "1.0.0"
-
-WPS, OWS = get_ElementMakerForVersion(VERSION)
-
-
-def _wps_input_file(identifier, filename):
-    return WPS.Input(
-        OWS.Identifier(identifier),
-        WPS.Reference(
-            WPS.Body("request body"),
-            {"{http://www.w3.org/1999/xlink}href": "file://" + filename},
-            method="POST",
-        ),
-    )
-
-
-def _wps_literal_input(identifier, value):
-    return WPS.Input(OWS.Identifier(identifier), WPS.Data(WPS.LiteralData(value)))
-
-
-@pytest.fixture(scope="module")
-def client():
-    return client_for(Service(processes=finch.processes.processes, cfgfiles=CFG_FILE))
-
-
-def _execute_process(client, identifier, inputs) -> xr.Dataset:
-    """Execute a process using the test client, and return the 'output_netcdf' output as an xarray.Dataset"""
-    request_doc = WPS.Execute(
-        OWS.Identifier(identifier), WPS.DataInputs(*inputs), version="1.0.0"
-    )
-    response = client.post_xml(doc=request_doc)
-    assert_response_success(response)
-
-    execution = WPSExecution()
-    execution.parseResponse(response.xml)
-
-    ds = _get_output_dataset(execution)
-
-    return ds
-
-
-def _get_output_dataset(execution):
-    ds = None
-    for output in execution.processOutputs:
-        if output.identifier == "output_netcdf":
-            path = output.reference.replace("file://", "")
-            ds = xr.open_dataset(path)
-    return ds
 
 
 def _get_output_standard_name(process_identifier):
@@ -70,8 +12,9 @@ def _get_output_standard_name(process_identifier):
 
 def test_tg_mean(client, tas_dataset):
     identifier = "tg_mean"
-    inputs = [_wps_input_file("tas", tas_dataset)]
-    ds = _execute_process(client, identifier, inputs)
+    inputs = [wps_input_file("tas", tas_dataset)]
+    outputs = execute_process(client, identifier, inputs)
+    ds = xr.open_dataset(outputs[0])
 
     assert ds.tg_mean.standard_name == _get_output_standard_name(identifier)
 
@@ -79,10 +22,11 @@ def test_tg_mean(client, tas_dataset):
 def test_heat_wave_frequency(client, tasmin_dataset, tasmax_dataset):
     identifier = "heat_wave_frequency"
     inputs = [
-        _wps_input_file("tasmax", tasmax_dataset),
-        _wps_input_file("tasmin", tasmin_dataset),
+        wps_input_file("tasmax", tasmax_dataset),
+        wps_input_file("tasmin", tasmin_dataset),
     ]
-    ds = _execute_process(client, identifier, inputs)
+    outputs = execute_process(client, identifier, inputs)
+    ds = xr.open_dataset(outputs[0])
 
     assert ds.heat_wave_frequency.standard_name == _get_output_standard_name(identifier)
 
@@ -90,9 +34,10 @@ def test_heat_wave_frequency(client, tasmin_dataset, tasmax_dataset):
 def test_heat_wave_index(client, tasmax_dataset):
     identifier = "hwi_{thresh}"
     inputs = [
-        _wps_input_file("tasmax", tasmax_dataset),
-        _wps_literal_input("thresh", "30"),
+        wps_input_file("tasmax", tasmax_dataset),
+        wps_literal_input("thresh", "30 degC"),
     ]
-    ds = _execute_process(client, identifier, inputs)
+    outputs = execute_process(client, identifier, inputs)
+    ds = xr.open_dataset(outputs[0])
 
-    assert ds.hwi_30.standard_name == _get_output_standard_name(identifier)
+    assert ds["hwi_30 degC"].standard_name == _get_output_standard_name(identifier)
