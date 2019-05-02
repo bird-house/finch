adf72ef4102e0d45b554299d7dc44f520cc7f795
David Caron <dcaron05@gmail.com>
2019-05-01 23:39:18 -0400

    take chunks for the try_opendap function

--- finch/processes/base.py
+++ finch/processes/base.py
@@ -3,47 +3,41 @@ from dask.diagnostics.progress import format_time
 from pywps import Process
 from sentry_sdk import configure_scope
 import xarray as xr
-import requests
 import logging
 import os
+
+from finch.processes.utils import is_opendap_url
 
 LOGGER = logging.getLogger("PYWPS")
 
 
 class FinchProcess(Process):
-
-    def try_opendap(self, input):
+    def try_opendap(self, input, chunks=None):
         """Try to open the file as an OPeNDAP url and chunk it. If OPeNDAP fails, access the file directly. In both
         cases, return an xarray.Dataset.
         """
         url = input.url
-        if url and not url.startswith("file"):
-            r = requests.get(url + ".dds")
-            if r.status_code == 200 and r.content.decode().startswith("Dataset"):
-                ds = xr.open_dataset(url)
-                chunks = chunk_dataset(ds, max_size=1000000)
-                ds = ds.chunk(chunks)
-                self.write_log("Opened dataset as an OPeNDAP url: {}".format(url))
-                return ds
+        if is_opendap_url(url):
+            ds = xr.open_dataset(url, chunks=chunks)
+            if not chunks:
+                ds = ds.chunk(chunk_dataset(ds, max_size=1000000))
+            self.write_log("Opened dataset as an OPeNDAP url: {}".format(url))
+        else:
+            self.write_log("Downloading dataset for url: {}".format(url))
+            # Accessing the file property loads the data in the data property
+            # and writes it to disk
+            ds = xr.open_dataset(input.file)
 
-        self.write_log("Downloading dataset for url: {}".format(url))
-
-        # Accessing the file property loads the data in the data property
-        # and writes it to disk
-        ds = xr.open_dataset(input.file)
-
-        # We need to cleanup the data property, otherwise it will be
-        # written in the database and to the output status xml file
-        # and it can get too large.
-        input._data = ""
         return ds
 
     def log_file_path(self):
-        return os.path.join(self.workdir, 'log.txt')
+        return os.path.join(self.workdir, "log.txt")
 
-    def write_log(self, message):
+    def write_log(self, message, response=None, percentage=None):
         open(self.log_file_path(), "a").write(message + "\n")
         LOGGER.info(message)
+        if response:
+            response.update_status(message, status_percentage=percentage)
 
     def sentry_configure_scope(self, request):
         """Add additional data to sentry error messages.
@@ -92,9 +86,9 @@ class FinchProgress(ProgressBar):
         start = self._start_percentage / 100
 
         frac += start - frac * start
-        bar = '#' * int(self._width * frac)
+        bar = "#" * int(self._width * frac)
         percent = int(100 * frac)
         elapsed = format_time(elapsed)
-        msg = '[{0:<{1}}] | {2}% Done | {3}'.format(bar, self._width, percent, elapsed)
+        msg = "[{0:<{1}}] | {2}% Done | {3}".format(bar, self._width, percent, elapsed)
 
         self._logging_function(msg, percent)
