040920d63202782b79258813d00f9a069827450e
David Huard <huard.david@ouranos.ca>
2019-09-16 17:08:23 -0400

    added one test using thredds dap inputs for subsetting.

--- tests/test_wps_xsubsetpoint.py
+++ tests/test_wps_xsubsetpoint.py
@@ -1,7 +1,7 @@
 from pywps import Service
 from pywps.tests import client_for, assert_response_success
 
-from .common import get_output, CFG_FILE
+from .common import get_output, get_metalinks, CFG_FILE
 from finch.processes import SubsetGridPointProcess
 import xarray as xr
 
@@ -24,3 +24,29 @@ def test_wps_xsubsetpoint(tas_dataset):
     ds = xr.open_dataset(out['output'][6:])
     assert ds.lat == 2
     assert ds.lon == 3
+
+
+def test_thredds():
+    import lxml.etree
+
+    client = client_for(Service(processes=[SubsetGridPointProcess()], cfgfiles=CFG_FILE))
+    fn1 = "https://pavics.ouranos.ca/twitcher/ows/proxy/thredds/dodsC/birdhouse/cmip5/MRI/rcp85/fx/atmos/r0i0p0" \
+          "/sftlf/sftlf_fx_MRI-CGCM3_rcp85_r0i0p0.nc"
+    fn2 = "https://pavics.ouranos.ca/twitcher/ows/proxy/thredds/dodsC/birdhouse/cmip5/MRI/rcp85/fx/atmos/r0i0p0/orog" \
+          "/orog_fx_MRI-CGCM3_rcp85_r0i0p0.nc"
+
+    datainputs = "resource=files@xlink:href={fn1};" \
+        "resource=files@xlink:href={fn2};" \
+        "lat={lat};"\
+        "lon={lon};".format(fn1=fn1, fn2=fn2, lat=45., lon=150.)
+
+    resp = client.get(
+        "?service=WPS&request=Execute&version=1.0.0&identifier=subset_gridpoint&datainputs={}".format(
+            datainputs))
+
+    assert_response_success(resp)
+    out = get_output(resp.xml)
+    links = get_metalinks(lxml.etree.fromstring(out['ref'].encode()))
+    assert len(links) == 2
+
+
