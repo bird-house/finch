d69a76d368759fd20f898f246e021d8ac6237e14
Trevor James Smith <trevor_smith@live.com>
2020-01-16 15:36:39 -0500

    use pip in docker image

--- travis-ci-local.sh
+++ travis-ci-local.sh
@@ -25,7 +25,7 @@ conda create -n finch python=$TRAVIS_PYTHON_VERSION
 conda env update -f environment.yml
 source activate finch
 conda install pytest flake8
-python setup.py install
+pip install -e .
 finch start --daemon --bind-host 0.0.0.0 --port 5000
 sleep 2
 pytest -m \"not online\"
