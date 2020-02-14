1e9b8dcbf1d99882732f344e98582c11a9af8689
David Caron <david.caron@crim.ca>
2020-02-13 22:27:28 -0500

    add test for ensemble heat wave frequency bbox

--- tests/test_wps_ensemble.py
+++ tests/test_wps_ensemble.py
@@ -13,7 +13,7 @@ def mock_datasets(monkeypatch):
     mock_local_datasets(monkeypatch, filenames=filenames)
 
 
-def test_ensemble_heatwave_frequency(mock_datasets, client):
+def test_ensemble_heatwave_frequency_grid_point(mock_datasets, client):
     # --- given ---
     identifier = "ensemble_grid_point_heat_wave_frequency"
     inputs = [
@@ -46,5 +46,47 @@ def test_ensemble_heatwave_frequency(mock_datasets, client):
         if k not in "lat lon realization time".split()
     }
     assert ensemble_variables
-    for v in ensemble_variables.values():
-        assert v.shape == (1, 4)
+    for var in ensemble_variables.values():
+        variable_dims = {d: s for d, s in zip(var.dimensions, var.shape)}
+        assert variable_dims == {"region": 1, "time": 4}
+
+
+def test_ensemble_heatwave_frequency_bbox(mock_datasets, client):
+    # --- given ---
+    identifier = "ensemble_bbox_heat_wave_frequency"
+    inputs = [
+        wps_literal_input("lat0", "46.0"),
+        wps_literal_input("lat1", "46.2"),
+        wps_literal_input("lon0", "-73.0"),
+        wps_literal_input("lon1", "-72.8"),
+        wps_literal_input("rcp", "rcp26"),
+        wps_literal_input("thresh_tasmin", "22.0 degC"),
+        wps_literal_input("thresh_tasmax", "30 degC"),
+        wps_literal_input("window", "3"),
+        wps_literal_input("freq", "MS"),
+        wps_literal_input("output_format", "netcdf"),
+    ]
+
+    # --- when ---
+    outputs = execute_process(client, identifier, inputs, output_names=["output"])
+
+    # --- then ---
+    assert len(outputs) == 1
+    ds = Dataset(outputs[0])
+    dims = {d.name: d.size for d in ds.dimensions.values()}
+    assert dims == {
+        "realization": 1,
+        "lat": 2,
+        "lon": 2,
+        "time": 4,  # there are roughly 4 months in the test datasets
+    }
+
+    ensemble_variables = {
+        k: v
+        for k, v in ds.variables.items()
+        if k not in "lat lon realization time".split()
+    }
+    assert ensemble_variables
+    for var in ensemble_variables.values():
+        variable_dims = {d: s for d, s in zip(var.dimensions, var.shape)}
+        assert variable_dims == {"time": 4, "lat": 2, "lon": 2}
