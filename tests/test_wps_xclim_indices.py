cf1d5b7f46ddd6ae49f25a5d534086daaa07687f
David Caron <david.caron@crim.ca>
2020-02-19 15:35:40 -0500

    more tests for drf_filename

--- tests/test_wps_xclim_indices.py
+++ tests/test_wps_xclim_indices.py
@@ -2,12 +2,13 @@ from finch.processes.wps_xclim_indices import XclimIndicatorBase
 from inspect import signature
 
 import xarray as xr
-import xclim
+import pandas as pd
 
 import finch
 import finch.processes
 from finch.processes.wps_base import make_xclim_indicator_process
 from tests.utils import execute_process, wps_input_file, wps_literal_input
+from pathlib import Path
 
 
 def _get_output_standard_name(process_identifier):
@@ -17,14 +18,9 @@ def _get_output_standard_name(process_identifier):
 
 
 def test_indicators_processes_discovery():
-    indicators = finch.processes.get_indicators(xclim.atmos)
-    indicator_names = [i.identifier for i in indicators]
-    processes = [
-        make_xclim_indicator_process(ind, "Process", XclimIndicatorBase)
-        for ind in indicators
-    ]
-    for name, indicator, process in zip(indicator_names, indicators, processes):
-        assert name == process.identifier
+    for indicator in finch.processes.indicators:
+        process = make_xclim_indicator_process(indicator, "Process", XclimIndicatorBase)
+        assert indicator.identifier == process.identifier
         sig = signature(indicator.compute)
         # the phase parameter is set by a partial function in xclim, so there is
         # no input necessary from the user in the WPS process
@@ -41,13 +37,15 @@ def test_processes(client, netcdf_datasets):
         for ind in indicators
     ]
     literal_inputs = {
-        "freq": "YS",
+        "freq": "MS",
         "window": "3",
     }
     keep_defaults = ["thresh", "thresh_tasmin", "thresh_tasmax"]
 
     # Todo: remove me when xclim.run_length.first_run_ufunc is fixed
     processes = [p for p in processes if p.identifier != "freshet_start"]
+
+    attrs = xr.open_dataset(list(netcdf_datasets.values())[0], decode_times=False).attrs
 
     for process in processes:
         inputs = []
@@ -64,7 +62,21 @@ def test_processes(client, netcdf_datasets):
         outputs = execute_process(client, process.identifier, inputs)
         ds = xr.open_dataset(outputs[0])
         output_variable = list(ds.data_vars)[0]
+
         assert getattr(ds, output_variable).standard_name == process.xci.standard_name
+
+        model = attrs["driving_model_id"]
+        experiment = attrs["driving_experiment_id"].replace(",", "+")
+        ensemble = (
+            f"r{attrs['driving_realization']}"
+            f"i{attrs['driving_initialization_method']}"
+            f"p{attrs['driving_physics_version']}"
+        )
+        date_start = pd.to_datetime(str(ds.time[0].values))
+        date_end = pd.to_datetime(str(ds.time[-1].values))
+
+        expected = f"{output_variable}_{model}_{experiment}_{ensemble}_{date_start:%Y%m%d}-{date_end:%Y%m%d}.nc"
+        assert Path(outputs[0]).name == expected
 
 
 def test_heat_wave_frequency_window_thresh_parameters(client, netcdf_datasets):
