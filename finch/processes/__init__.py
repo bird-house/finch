e3a6f989ef71c8958ab3cb7b7a0a1c5a9160730c
David Caron <david.caron@crim.ca>
2020-03-02 12:04:53 -0500

    temporary fix until xclim 0.15 is released

--- finch/processes/__init__.py
+++ finch/processes/__init__.py
@@ -3,7 +3,12 @@ import logging
 from pywps.configuration import get_config_value
 import xclim
 import xclim.atmos
-from xclim.utils import Indicator
+
+try:
+    from xclim.indicator import Indicator
+except ImportError:
+    # Todo: remove me when xclim 0.15 is released
+    from xclim.utils import Indicator
 
 from .ensemble_utils import uses_accepted_netcdf_variables
 from .wps_base import make_xclim_indicator_process
