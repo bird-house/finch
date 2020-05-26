7149fa0c25d0b7f47f2c453e97e0724a69fce187
Long Vu <vu.long@ouranos.ca>
2020-05-26 17:01:14 -0400

    RtD: add numba to mock import
    https://readthedocs.org/api/v2/build/11123466.txt
    
      File "/home/docs/checkouts/readthedocs.org/user_builds/finch/conda/fix-rtd-build/lib/python3.8/site-packages/xclim/indices/fwi.py", line 96, in <module>
        from numba import jit
      File "/home/docs/checkouts/readthedocs.org/user_builds/finch/conda/fix-rtd-build/lib/python3.8/site-packages/numba/__init__.py", line 15, in <module>
        from . import config, errors, _runtests as runtests, types
      File "/home/docs/checkouts/readthedocs.org/user_builds/finch/conda/fix-rtd-build/lib/python3.8/site-packages/numba/types/__init__.py", line 102, in <module>
        char = _make_signed(np.byte)
      File "/home/docs/checkouts/readthedocs.org/user_builds/finch/conda/fix-rtd-build/lib/python3.8/site-packages/numba/types/__init__.py", line 99, in <lambda>
        _make_signed = lambda x: globals()["int%d" % (np.dtype(x).itemsize * 8)]
    TypeError: unsupported operand type(s) for *: 'itemsize' and 'int'

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -51,7 +51,7 @@ extensions = [
 autodoc_mock_imports = ["numpy", "xarray", "fiona", "rasterio", "shapely",
                         "osgeo", "geopandas", "pandas", "statsmodels",
                         "affine", "rasterstats", "spotpy", "matplotlib",
-                        "scipy", "unidecode", "sentry_sdk", "dask"]
+                        "scipy", "unidecode", "sentry_sdk", "dask", "numba"]
 
 # We are using mock imports in readthedocs, so probably safer to not run the notebooks
 nbsphinx_execute = 'never'
