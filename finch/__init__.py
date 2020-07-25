bird-house/finch

commit 6dec5ea0f08446ebf3a9565626bab43e3b4fc373

Update from cookiecutter template (#67)
* update from cookiecutter template

finch/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
-"""Top-level package for Finch."""
+"""Top-level package for finch."""
 
-from .__version__ import __author__, __email__, __version__
+from .__version__ import __author__, __email__, __version__  # noqa: F401
 
-from .wsgi import application
+from .wsgi import application  # noqa: F401

.travis.yml
@@ -1,6 +1,8 @@
+dist: xenial # required for Python >= 3.7
 language: python
 python:
-  - "3.6"
+  - "3.7"
+  - "3.8"
 os:
   - linux
 #  - osx
@@ -12,16 +14,14 @@ install:
   - export PATH="$HOME/miniconda/bin:$PATH"
   - hash -r
   - conda config --set always_yes yes --set changeps1 no
+  - conda install setuptools
   - conda update -q conda
-  # Useful for debugging any issues with conda
-  - conda info -a
-  - conda env create -f environment.yml
+  - conda create -n finch -c conda-forge python=$TRAVIS_PYTHON_VERSION
+  - conda env update -f environment.yml
   - source activate finch
   # Packages for testing
   - conda install pytest flake8
-  - pip install --no-deps -e .
-  # install some python 2.7 backports required by pywps... necessary for `make start` to work
-  - pip install flufl.enum pathlib
+  - python setup.py install
 before_script:
   # Start WPS service on port 5000 on 0.0.0.0
   - make start

Makefile
@@ -1,13 +1,6 @@
-# Application
-APP_ROOT := $(CURDIR)
+# Configuration
+APP_ROOT := $(abspath $(lastword $(MAKEFILE_LIST))/..)
 APP_NAME := finch
-
-# Anaconda
-CONDA := $(shell command -v conda 2> /dev/null)
-ANACONDA_HOME := $(shell conda info --base 2> /dev/null)
-CONDA_ENV := $(APP_NAME)
-
-TEMP_FILES := *.egg-info *.log *.sqlite
 
 # end of configuration
 
@@ -18,110 +11,119 @@ all: help
 
 .PHONY: help
 help:
-	@echo "Please use \`make <target>' where <target> is one of"
-	@echo "  help        to print this help message. (Default)"
-	@echo "  install     to install $(APP_NAME) by running 'python setup.py develop'."
-	@echo "  start       to start $(APP_NAME) service as daemon (background process)."
-	@echo "  stop        to stop $(APP_NAME) service."
-	@echo "  status      to show status of $(APP_NAME) service."
-	@echo "  clean       to remove *all* files that are not controlled by 'git'. WARNING: use it *only* if you know what you do!"
+	@echo "Please use 'make <target>' where <target> is one of:"
+	@echo "  help              to print this help message. (Default)"
+	@echo "  install           to install app by running 'pip install -e .'"
+	@echo "  develop           to install with additional development requirements."
+	@echo "  start             to start $(APP_NAME) service as daemon (background process)."
+	@echo "  stop              to stop $(APP_NAME) service."
+	@echo "  restart           to restart $(APP_NAME) service."
+	@echo "  status            to show status of $(APP_NAME) service."
+	@echo "  clean             to remove all files generated by build and tests."
 	@echo "\nTesting targets:"
-	@echo "  test        to run tests (but skip long running tests)."
-	@echo "  testall     to run all tests (including long running tests)."
-	@echo "  pep8        to run pep8 code style checks."
+	@echo "  test              to run tests (but skip long running tests)."
+	@echo "  test-all          to run all tests (including long running tests)."
+	@echo "  lint              to run code style checks with flake8."
 	@echo "\nSphinx targets:"
-	@echo "  docs        to generate HTML documentation with Sphinx."
-
-## Anaconda targets
-
-.PHONY: check_conda
-check_conda:
-ifndef CONDA
-		$(error "Conda is not available. Please install miniconda: https://conda.io/miniconda.html")
-endif
-
-.PHONY: conda_env
-conda_env: check_conda
-	@echo "Updating conda environment $(CONDA_ENV) ..."
-	"$(CONDA)" env update -n $(CONDA_ENV) -f environment.yml
-
-.PHONY: envclean
-envclean: check_conda
-	@echo "Removing conda env $(CONDA_ENV)"
-	@-"$(CONDA)" remove -n $(CONDA_ENV) --yes --all
+	@echo "  docs              to generate HTML documentation with Sphinx."
+	@echo "\nDeployment targets:"
+	@echo "  dist              to build source and wheel package."
 
 ## Build targets
 
-.PHONY: bootstrap
-bootstrap: check_conda conda_env bootstrap_dev
-	@echo "Bootstrap ..."
-
-.PHONY: bootstrap_dev
-bootstrap_dev:
-	@echo "Installing development requirements for tests and docs ..."
-	@-bash -c "$(CONDA) install -y -n $(CONDA_ENV) -c conda-forge pytest flake8 sphinx bumpversion"
-	@-bash -c "source $(ANACONDA_HOME)/bin/activate $(CONDA_ENV) && pip install -r requirements_dev.txt"
-
 .PHONY: install
-install: bootstrap
+install:
 	@echo "Installing application ..."
-	@-bash -c "source $(ANACONDA_HOME)/bin/activate $(CONDA_ENV) && python setup.py develop"
+	@-bash -c 'pip install -e .'
 	@echo "\nStart service with \`make start'"
 
+.PHONY: develop
+develop:
+	@echo "Installing development requirements for tests and docs ..."
+	@-bash -c 'pip install -e ".[dev]"'
+
 .PHONY: start
-start: check_conda
+start:
 	@echo "Starting application ..."
-	@-bash -c "source $(ANACONDA_HOME)/bin/activate $(CONDA_ENV) && $(APP_NAME) start -d"
+	@-bash -c "$(APP_NAME) start -d"
 
 .PHONY: stop
-stop: check_conda
+stop:
 	@echo "Stopping application ..."
-	@-bash -c "source $(ANACONDA_HOME)/bin/activate $(CONDA_ENV) && $(APP_NAME) stop"
+	@-bash -c "$(APP_NAME) stop"
+
+.PHONY: restart
+restart: stop start
+	@echo "Restarting application ..."
 
 .PHONY: status
-status: check_conda
+status:
 	@echo "Show status ..."
-	@-bash -c "source $(ANACONDA_HOME)/bin/activate $(CONDA_ENV) && $(APP_NAME) status"
+	@-bash -c "$(APP_NAME) status"
 
 .PHONY: clean
-clean: srcclean envclean
-	@echo "Cleaning generated files ..."
-	@-for i in $(TEMP_FILES); do \
-  	test -e $$i && rm -v -rf $$i; \
-  done
+clean: clean-build clean-pyc clean-test ## remove all build, test, coverage and Python artifacts
 
-.PHONY: srcclean
-srcclean:
-	@echo "Removing *.pyc files ..."
-	@-find $(APP_ROOT) -type f -name "*.pyc" -print | xargs rm
+.PHONY: clean-build
+clean-build:
+	@echo "Remove build artifacts ..."
+	@-rm -fr build/
+	@-rm -fr dist/
+	@-rm -fr .eggs/
+	@-find . -name '*.egg-info' -exec rm -fr {} +
+	@-find . -name '*.egg' -exec rm -f {} +
+	@-find . -name '*.log' -exec rm -fr {} +
+	@-find . -name '*.sqlite' -exec rm -fr {} +
 
-.PHONY: distclean
-distclean: clean
-	@echo "Cleaning ..."
+.PHONY: clean-pyc
+clean-pyc:
+	@echo "Remove Python file artifacts ..."
+	@-find . -name '*.pyc' -exec rm -f {} +
+	@-find . -name '*.pyo' -exec rm -f {} +
+	@-find . -name '*~' -exec rm -f {} +
+	@-find . -name '__pycache__' -exec rm -fr {} +
+
+.PHONY: clean-test
+clean-test:
+	@echo "Remove test artifacts ..."
+	@-rm -fr .pytest_cache
+
+.PHONY: clean-dist
+clean-dist: clean
+	@echo "Run 'git clean' ..."
 	@git diff --quiet HEAD || echo "There are uncommited changes! Not doing 'git clean' ..."
-	@-git clean -dfx -e *.bak -e custom.cfg
+	@-git clean -dfx
 
 ## Test targets
 
 .PHONY: test
 test: check_conda
 	@echo "Running tests (skip slow and online tests) ..."
-	@-bash -c "source $(ANACONDA_HOME)/bin/activate $(CONDA_ENV);pytest -v -m 'not slow and not online'"
+	@bash -c 'pytest -v -m "not slow and not online" tests/'
 
-.PHONY: testall
-testall: check_conda
+.PHONY: test-all
+test-all:
 	@echo "Running all tests (including slow and online tests) ..."
-	@-bash -c "source $(ANACONDA_HOME)/bin/activate $(CONDA_ENV) && pytest -v"
+	@bash -c 'pytest -v tests/'
 
-.PHONY: pep8
-pep8: check_conda
-	@echo "Running pep8 code style checks ..."
-	@-bash -c "source $(ANACONDA_HOME)/bin/activate $(CONDA_ENV) && flake8"
+.PHONY: lint
+lint:
+	@echo "Running flake8 code style checks ..."
+	@bash -c 'flake8'
 
-##  Sphinx targets
+## Sphinx targets
 
 .PHONY: docs
 docs: check_conda
 	@echo "Generating docs with Sphinx ..."
-	@-bash -c "source $(ANACONDA_HOME)/bin/activate $(CONDA_ENV);$(MAKE) -C $@ clean html"
-	@echo "open your browser: firefox docs/build/html/index.html"
+	@-bash -c '$(MAKE) -C $@ clean html'
+	@echo "open your browser: open file://$(APP_ROOT)/docs/build/html/index.html"
+
+## Deployment targets
+
+.PHONY: dist
+dist: clean
+	@echo "Builds source and wheel package ..."
+	@-python setup.py sdist
+	@-python setup.py bdist_wheel
+	ls -l dist

docker-compose.yml
@@ -1,12 +1,10 @@
-version: '3'
+version: "3"
 services:
-  emu:
+  wps:
     build: .
     image: bird-house/finch
     ports:
       - "5000:5000"
-
-
 # docker-compose build
 # docker-compose up
 # docker-compose down

docs/source/conf.py
@@ -51,7 +51,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'Finch'
+project = u'finch'
 copyright = u"2018, David Huard"
 author = u"David Huard"
 
@@ -141,7 +141,7 @@ latex_elements = {
 # [howto, manual, or own class]).
 latex_documents = [
     (master_doc, 'finch.tex',
-     u'Finch Documentation',
+     u'finch Documentation',
      u'David Huard', 'manual'),
 ]
 
@@ -152,7 +152,7 @@ latex_documents = [
 # (source start file, name, description, authors, manual section).
 man_pages = [
     (master_doc, 'finch',
-     u'Finch Documentation',
+     u'finch Documentation',
      [author], 1)
 ]
 
@@ -164,7 +164,7 @@ man_pages = [
 #  dir menu entry, description, category)
 texinfo_documents = [
     (master_doc, 'finch',
-     u'Finch Documentation',
+     u'finch Documentation',
      author,
      'finch',
      'One line description of project.',

finch/__version__.py
@@ -5,5 +5,5 @@
 # See: https://packaging.python.org/guides/single-sourcing-package-version
 
 __author__ = """David Huard"""
-__email__ = 'huard.david@ouranos.ca'
-__version__ = '0.3.1'
+__email__ = "huard.david@ouranos.ca"
+__version__ = "0.3.1"

finch/cli.py
@@ -12,7 +12,7 @@ from jinja2 import Environment, PackageLoader
 from pywps import configuration
 
 from . import wsgi
-from six.moves.urllib.parse import urlparse
+from urllib.parse import urlparse
 
 PID_FILE = os.path.abspath(os.path.join(os.path.curdir, "pywps.pid"))
 
@@ -88,8 +88,8 @@ def _run(application, bind_host=None, daemon=False):
         application=application,
         use_debugger=False,
         use_reloader=False,
-        threaded=False,
-        processes=2,
+        threaded=True,
+        # processes=2,
         use_evalex=not daemon,
         static_files=static_files)
 

finch/default.cfg
@@ -1,9 +1,9 @@
 [metadata:main]
-identification_title = Finch
+identification_title = finch
 identification_abstract = A Web Processing Service for Climate Indicators
 identification_keywords = =PyWPS, WPS, OGC, processing, birdhouse, finch, demo
 identification_keywords_type = theme
-provider_name = Finch
+provider_name = finch
 provider_url=http://finch.readthedocs.org/en/latest/
 
 [server]

commit b5e74b1074076eeb670d38004c891020f740dbac

doc: uppercase "finch" in several locations to match cookiecutter
Commit 6dec5ea0f08446ebf3a9565626bab43e3b4fc373 accidentally lowercase
the word "finch".

finch/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-"""Top-level package for finch."""
+"""Top-level package for Finch."""
 
 from .__version__ import __author__, __email__, __version__  # noqa: F401
 