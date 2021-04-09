f067b1d81f5c4c8f0a9a38aac28f008b79138651
Phobos <bourgault.pascal@ouranos.ca>
2021-04-09 17:06:17 -0400

    Fix linting

--- finch/processes/wps_ensemble_indices_polygon.py
+++ finch/processes/wps_ensemble_indices_polygon.py
@@ -27,7 +27,8 @@ class XclimEnsemblePolygonBase(FinchProcess):
             )
 
         attrs = self.xci.json()
-        xci_inputs = convert_xclim_inputs_to_pywps(attrs["parameters"], self.xci.identifier) + wpsio.xclim_common_options
+        xci_inputs = convert_xclim_inputs_to_pywps(attrs["parameters"], self.xci.identifier)
+        xci_inputs.extend(wpsio.xclim_common_options)
         self.xci_inputs_identifiers = [i.identifier for i in xci_inputs]
 
         inputs = [
