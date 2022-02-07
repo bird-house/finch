55dd8002457d18120d3e6f0aac2e5157eabbef33
Pascal Bourgault <bourgault.pascal@ouranos.ca>
2022-02-07 17:57:34 -0500

    Add slugify to mock imports

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -72,6 +72,7 @@ if os.environ.get('READTHEDOCS') == 'True':
         "sentry_sdk",
         "shapely",
         "siphon",
+        "slugify",
         "spotpy",
         "statsmodels",
         "unidecode",
