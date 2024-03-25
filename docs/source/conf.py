0d65f7635de863e3a08758ae2ada81e7c9556f73
Zeitsperre <10819524+Zeitsperre@users.noreply.github.com>
2024-03-25 12:02:39 -0400

    short-version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -132,7 +132,7 @@ author = "David Huard"
 # the built documents.
 #
 # The short X.Y version.
-version = "0.12.0"
+version = "0.12.0".split("-")[0]
 # The full version, including alpha/beta/rc tags.
 release = "0.12.0"
 
