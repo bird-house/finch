9519a986cda05ebf1da8ab8950c879b0e40eaa76
Zeitsperre <10819524+Zeitsperre@users.noreply.github.com>
2024-05-31 09:59:48 -0400

    replace unidecod with anyascii

--- finch/processes/wps_ensemble_indices_polygon.py
+++ finch/processes/wps_ensemble_indices_polygon.py
@@ -1,7 +1,7 @@
 # noqa: D100
 import logging
 
-from unidecode import unidecode
+from anyascii import anyascii
 
 from . import wpsio
 from .ensemble_utils import ensemble_common_handler
@@ -58,8 +58,8 @@ class XclimEnsemblePolygonBase(FinchProcess):
             self._handler,
             identifier=identifier,
             version="0.1",
-            title=unidecode(self.xci.title),
-            abstract=unidecode(self.xci.abstract),
+            title=anyascii(self.xci.title),
+            abstract=anyascii(self.xci.abstract),
             inputs=inputs,
             outputs=outputs,
             status_supported=True,
